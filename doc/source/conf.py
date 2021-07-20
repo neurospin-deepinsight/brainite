e0da2132608ba0988d440c4442c8956127b45d97
Antoine Grigis <antoine.grigis@cea.fr>
2021-07-20 17:02:09 +0200

    doc/source/conf: mock visdom.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,6 +15,18 @@ import subprocess
 from distutils.version import LooseVersion
 import sphinx
 import pysphinxdoc
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        print(cls, name)
+        mock = MagicMock()
+        mock().get_window_data.return_value = "{}"
+        return mock
+MOCK_MODULES = ["visdom"]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 installdir = os.path.abspath("../..")
